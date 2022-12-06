e4cca3b0f44dbed5af1560cbfb2820a6da86a493
Maxim Sakharov <sakharov.ma@sberleasing.ru>
2022-12-05 18:31:14 -0600

    show company listings

--- app/src/main/java/com/example/stockmarketapp/MainActivity.kt
+++ app/src/main/java/com/example/stockmarketapp/MainActivity.kt
@@ -6,11 +6,10 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.stockmarketapp.presentation.company_listings.NavGraphs
 import com.example.stockmarketapp.ui.theme.StockMarketAppTheme
+import com.ramcosta.composedestinations.DestinationsNavHost
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -24,7 +23,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-
+                    DestinationsNavHost(navGraph = NavGraphs.root)
                 }
             }
         }
