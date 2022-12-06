925c6ddf36c33525394d0131fbc313d70bdbb273
Maxim Sakharov <sakharov.ma@sberleasing.ru>
2022-12-06 11:44:26 -0600

    implement data layer for company info screen

--- app/src/main/java/com/example/stockmarketapp/data/remote/StockApi.kt
+++ app/src/main/java/com/example/stockmarketapp/data/remote/StockApi.kt
@@ -1,5 +1,6 @@
 package com.example.stockmarketapp.data.remote
 
+import com.example.stockmarketapp.data.remote.dto.CompanyInfoDto
 import okhttp3.ResponseBody
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -10,7 +11,20 @@ interface StockApi {
     @GET("query?function=LISTING_STATUS")
     suspend fun getListings(
         @Query("apikey") apikey: String = API_KEY
-    ):ResponseBody
+    ): ResponseBody
+
+    @GET("query?function=TIME_SERIES_INTRADAY&interval=60min&datatype=csv")
+    suspend fun getIntradayInfo(
+        @Query("symbol") symbol: String,
+        @Query("apikey") apikey: String = API_KEY
+    ): ResponseBody
+
+    @GET("query?function=OVERVIEW")
+    suspend fun getCompanyInfo(
+        @Query("symbol") symbol: String,
+        @Query("apikey") apikey: String = API_KEY
+    ): CompanyInfoDto
+
 
     companion object {
         const val API_KEY = "94OUUUUAT33IS4AJ"
